fa27d50daa394e345b5524a27899a6ecbfdcb4e1
Rahul singh <rs618642@gmail.com>
2022-11-09 19:10:24 +0530

    Refactored Yopmail confirmation

--- src/test/java/www/logytalks/com/YopmailConfirm.java
+++ src/test/java/www/logytalks/com/YopmailConfirm.java
@@ -1,13 +1,19 @@
 package www.logytalks.com;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import Base.BasePage;
+import Base.ExtentManager;
+import pageObjects.SignInPage;
+
+@Listeners(resources.Listeners.class)
 
 public class YopmailConfirm extends BasePage {
 	public YopmailConfirm() throws IOException {
@@ -26,23 +32,31 @@ public class YopmailConfirm extends BasePage {
 		driver.quit();
 	}
 
-	@Test
-	public void emailConfirm() {
+	@Test(dataProvider = "getData")
+	public void emailConfirm(String email, String password, String CoPassword, String first_name, String last_name,
+			String foi) {
 
-		driver.findElement(By.cssSelector("#login")).sendKeys("rahulsingh103@yopmail.com");
+		ExtentManager.log("Starting Yopmail confirmation test");
+		driver.findElement(By.cssSelector("#login")).sendKeys(email);
 
 		driver.findElement(By.cssSelector("button")).click();
 
-		try {
-			driver.switchTo().frame("1");
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+		ExtentManager.pass("Reached inbox of " + email);
+		driver.switchTo().frame("ifmail");
 
-		try {
-			driver.findElement(By.linkText("Verify Email Address")).click();
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+		driver.findElement(By.linkText("Verify Email Address")).click();
+
+		driver.switchTo().parentFrame();
+
+		ArrayList<String> windowsHandles = new ArrayList<String>(driver.getWindowHandles());
+
+		driver.switchTo().window(windowsHandles.get(1));
+
+		SignInPage signin = new SignInPage(driver);
+
+		signin.getemailInput().sendKeys(email);
+		signin.getpasswordInput().sendKeys(password);
+		signin.getsubmitLoginBtn().click();
+
 	}
 }
